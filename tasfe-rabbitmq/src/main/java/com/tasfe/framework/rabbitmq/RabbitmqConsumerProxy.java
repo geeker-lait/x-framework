2e149b9a2c6eca9de5e0b409f183502f4da24e9b
liat.zhang <lait.zhang@gmail.com>
2017-11-30 15:54:13 +0800

    update rabbitmq

--- tasfe-rabbitmq/src/main/java/com/tasfe/framework/rabbitmq/RabbitmqConsumerProxy.java
+++ tasfe-rabbitmq/src/main/java/com/tasfe/framework/rabbitmq/RabbitmqConsumerProxy.java
@@ -1,12 +1,15 @@
 package com.tasfe.framework.rabbitmq;
 
 import com.rabbitmq.client.Channel;
+import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.InitializingBean;
 
+import java.io.IOException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -15,38 +18,103 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by Lait on 10/17/2016.
  */
+@Setter
 public class RabbitmqConsumerProxy implements ChannelAwareMessageListener, InitializingBean {
     private Logger logger = LoggerFactory.getLogger(RabbitmqConsumerProxy.class);
 
     private RabbitmqMessageProcesser rabbitmqMessageProcesser;
 
-    private int threadNum = 5;
+    private MessageConverter messageConverter;
+
+    /*private int threadNum = 5;
     private int maxQueueSize = 2000;
 
     private ExecutorService executorService = new ThreadPoolExecutor(threadNum,
             threadNum, 0L, TimeUnit.MILLISECONDS,
             new ArrayBlockingQueue<Runnable>(maxQueueSize),
-            new ThreadPoolExecutor.CallerRunsPolicy());
+            new ThreadPoolExecutor.CallerRunsPolicy());*/
 
     @Override
-    public void onMessage(final Message message, final Channel channel) throws Exception {
+    public void onMessage(final Message message, final Channel channel) {
         logger.info("开始处理消息: " + message.toString());
+
+        // 获取对象
+        Object msg = messageConverter.fromMessage(message);
+        try {
+            /**
+             * 消费者在开启acknowledge的情况下，对接收到的消息可以根据业务的需要异步对消息进行确认。
+             * 然而在实际使用过程中，由于消费者自身处理能力有限，从rabbitmq获取一定数量的消息后，希望rabbitmq不再将队列中的消息推送过来，
+             * 当对消息处理完后（即对消息进行了ack，并且有能力处理更多的消息）再接收来自队列的消息。
+             * 在这种场景下，我们可以通过设置basic.qos信令中的prefetch_count来达到这种效果。
+             */
+            //channel.basicQos(5);
+            /**We're about to tell the server to deliver us the messages from the queue.
+             * Since it will push us messages asynchronously,
+             * we provide a callback in the form of an object that will buffer the messages
+             * until we're ready to use them. That is what QueueingConsumer does.*/
+            //QueueingConsumer consumer = new QueueingConsumer(channel);
+            /**
+             * 把名字为orderQueue的Channel的值回调给QueueingConsumer,即使一个worker在处理消息的过程中停止了，这个消息也不会失效
+             */
+            //channel.basicConsume(messages.getMessageProperties().getConsumerQueue(), false, consumer);
+            //Cat.logMetricForCount("RabbitmqConsumerProxy.onMessage(final Message message, final Channel channel).count");
+            //trans.setStatus("0");
+            rabbitmqMessageProcesser.processMessage(msg);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (Exception ae) {
+            // trans.setStatus(ae);
+            try {
+                // 处理失败,调用nack，并将消息重新回到队列
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+            }
+        } finally {
+            //trans.complete();
+        }
+
+        /*
         executorService.execute(new Runnable() {
             @Override
             public void run() {
                 //Transaction trans = Cat.newTransaction("Service", "RabbitmqConsumerProxy.onMessage(final Message message, final Channel channel)");
+                // 强转对象
+                T msg = messageConverter.fromMessage(message);
                 try {
-                    rabbitmqMessageProcesser.processMessage(message, channel);
+                    *//**
+                     * 消费者在开启acknowledge的情况下，对接收到的消息可以根据业务的需要异步对消息进行确认。
+                     * 然而在实际使用过程中，由于消费者自身处理能力有限，从rabbitmq获取一定数量的消息后，希望rabbitmq不再将队列中的消息推送过来，
+                     * 当对消息处理完后（即对消息进行了ack，并且有能力处理更多的消息）再接收来自队列的消息。
+                     * 在这种场景下，我们可以通过设置basic.qos信令中的prefetch_count来达到这种效果。
+                     *//*
+                    //channel.basicQos(5);
+                    *//**We're about to tell the server to deliver us the messages from the queue.
+                     * Since it will push us messages asynchronously,
+                     * we provide a callback in the form of an object that will buffer the messages
+                     * until we're ready to use them. That is what QueueingConsumer does.*//*
+                    //QueueingConsumer consumer = new QueueingConsumer(channel);
+                    *//**
+                     * 把名字为orderQueue的Channel的值回调给QueueingConsumer,即使一个worker在处理消息的过程中停止了，这个消息也不会失效
+                     *//*
+                    //channel.basicConsume(messages.getMessageProperties().getConsumerQueue(), false, consumer);
                     //Cat.logMetricForCount("RabbitmqConsumerProxy.onMessage(final Message message, final Channel channel).count");
                     //trans.setStatus("0");
+                    rabbitmqMessageProcesser.processMessage(msg);
+
+                    channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
                 } catch (Exception ae) {
-                   // trans.setStatus(ae);
-                    throw ae;
+                    // trans.setStatus(ae);
+                    // 处理失败,调用nack，并将消息重新回到队列
+                    try {
+                        channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 } finally {
                     //trans.complete();
                 }
             }
-        });
+        });*/
 
     }
 
@@ -65,7 +133,7 @@ public class RabbitmqConsumerProxy implements ChannelAwareMessageListener, Initi
             }
         });
     }*/
-
+/*
     public int getThreadNum() {
         return threadNum;
     }
@@ -82,11 +150,19 @@ public class RabbitmqConsumerProxy implements ChannelAwareMessageListener, Initi
         this.maxQueueSize = maxQueueSize;
     }
 
+    public MessageConverter getMessageConverter() {
+        return messageConverter;
+    }
+
+    public void setMessageConverter(MessageConverter messageConverter) {
+        this.messageConverter = messageConverter;
+    }
+
     public RabbitmqMessageProcesser getRabbitmqMessageProcesser() {
         return rabbitmqMessageProcesser;
     }
 
     public void setRabbitmqMessageProcesser(RabbitmqMessageProcesser rabbitmqMessageProcesser) {
         this.rabbitmqMessageProcesser = rabbitmqMessageProcesser;
-    }
+    }*/
 }
--- tasfe-rabbitmq/src/main/java/com/tasfe/framework/rabbitmq/RabbitmqMessageProcesser.java
+++ tasfe-rabbitmq/src/main/java/com/tasfe/framework/rabbitmq/RabbitmqMessageProcesser.java
@@ -1,11 +1,9 @@
 package com.tasfe.framework.rabbitmq;
 
-import com.rabbitmq.client.Channel;
-import org.springframework.amqp.core.Message;
 
 /**
  * Created by Lait on 10/17/2016.
  */
-public interface RabbitmqMessageProcesser {
-    void processMessage(Message message, Channel channel);
+public interface RabbitmqMessageProcesser<T> {
+    void processMessage(T message);
 }
